e20e517e73d5279e6d269697218d41b8548c2c53
Mr Hery <heryintelt@gmail.com>
2023-06-25 12:22:54 +0800

    Update 404.php

---
+++ Systems/Apps/ccms/View/404.php
@@ -0,0 +1,13 @@
+
+
+<div class="contaier">
+	<div class="row">
+		<div class="col-md-12 text-center">
+			<h1>Oops! The Page You Looking For Is Not Available.</h1>
+			
+			<a href="<?= PORTAL ?>" class="btn btn-sm btn-primary">
+				Back to Home
+			</a>
+		</div>
+	</div>
+</div>
