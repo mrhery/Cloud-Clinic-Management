e2f470bce2d8b964b1c1e31a7cf85b548594ca33
Mr Hery <heryintelt@gmail.com>
2023-06-25 12:28:42 +0800

    Update 404.php

--- Systems/Apps/ccms/View/404.php
+++ Systems/Apps/ccms/View/404.php
@@ -2,7 +2,7 @@
 
 <div class="contaier">
 	<div class="row">
-		<div class="col-md-12 text-center">
+		<div class="col-md-12 text-center mt-5">
 			<h1>Oops! The Page You Looking For Is Not Available.</h1>
 			
 			<a href="<?= PORTAL ?>" class="btn btn-sm btn-primary">
