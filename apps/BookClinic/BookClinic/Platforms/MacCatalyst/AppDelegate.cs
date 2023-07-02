caaeb5f01f3be0c6faaa2be5c9b0fe2e657d58e0
Mr Hery <heryintelt@gmail.com>
2023-07-02 14:09:02 +0800

    app project created

---
+++ apps/BookClinic/BookClinic/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BookClinic;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
